cf2fc71512f24e2d8961c09aba4745df0b339a64
Olfens <johannes.olfens@accellence.de>
2024-10-23 09:19:09 +0200

    added debug info, fixed crashes by generating unique client ids

--- src/main/java/com/sintef/asam/MqttSourceConnectorTask.java
+++ src/main/java/com/sintef/asam/MqttSourceConnectorTask.java
@@ -23,6 +23,9 @@ import java.time.format.DateTimeFormatter;
 
 import org.bson.Document;
 
+import java.util.Collections;
+
+
 public class MqttSourceConnectorTask extends SourceTask implements MqttCallback {
 
     private MqttClient mqttClient;
@@ -31,19 +34,22 @@ public class MqttSourceConnectorTask extends SourceTask implements MqttCallback 
     private String mqttClientId;
     private String connectorName;
     private MqttSourceConnectorConfig connectorConfiguration;
-    private SSLSocketFactory sslSocketFactory;
-    BlockingQueue<SourceRecord> mqttRecordQueue = new LinkedBlockingQueue<SourceRecord>();
+    BlockingQueue<SourceRecord> mqttRecordQueue = new LinkedBlockingQueue<>();
     private static final Logger logger = LogManager.getLogger(MqttSourceConnectorTask.class);
 
     private void initMqttClient() {
-
         MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
         mqttConnectOptions.setMqttVersion(MqttConnectOptions.MQTT_VERSION_3_1_1);
-        mqttConnectOptions.setServerURIs(new String[] {connectorConfiguration.getString("mqtt.connector.broker.uri")});
+        mqttConnectOptions
+                .setServerURIs(new String[] { connectorConfiguration.getString("mqtt.connector.broker.uri") });
         mqttConnectOptions.setConnectionTimeout(connectorConfiguration.getInt("mqtt.connector.connection_timeout"));
         mqttConnectOptions.setKeepAliveInterval(connectorConfiguration.getInt("mqtt.connector.keep_alive"));
         mqttConnectOptions.setCleanSession(connectorConfiguration.getBoolean("mqtt.connector.clean_session"));
-        mqttConnectOptions.setKeepAliveInterval(connectorConfiguration.getInt("mqtt.connector.connection_timeout"));
+        mqttConnectOptions.setUserName(connectorConfiguration.getString("mqtt.connector.username"));
+        mqttConnectOptions
+                .setPassword(connectorConfiguration.getPassword("mqtt.connector.password").value().toCharArray());
+        mqttConnectOptions.setAutomaticReconnect(true); 
+		mqttConnectOptions.setKeepAliveInterval(connectorConfiguration.getInt("mqtt.connector.connection_timeout"));
         if (connectorConfiguration.getBoolean("mqtt.connector.ssl")) {
             logger.info("SSL TRUE for MqttSourceConnectorTask: '{}, and mqtt client: '{}'.", connectorName, mqttClientId);
             try {
@@ -60,25 +66,30 @@ public class MqttSourceConnectorTask extends SourceTask implements MqttCallback 
         } else {
             logger.info("SSL FALSE for MqttSourceConnectorTask: '{}, and mqtt client: '{}'.", connectorName, mqttClientId);
         }
-
+		
         try {
-            mqttClient = new MqttClient(connectorConfiguration.getString("mqtt.connector.broker.uri"), mqttClientId, new MemoryPersistence());
+            mqttClientId = MqttAsyncClient.generateClientId(); 
+            mqttClient = new MqttClient(connectorConfiguration.getString("mqtt.connector.broker.uri"), mqttClientId,
+                    new MemoryPersistence());
             mqttClient.setCallback(this);
             mqttClient.connect(mqttConnectOptions);
-            logger.info("SUCCESSFULL MQTT CONNECTION for AsamMqttSourceConnectorTask: '{}, and mqtt client: '{}'.", connectorName, mqttClientId);
+            logger.info("SUCCESSFUL MQTT CONNECTION for AsamMqttSourceConnectorTask: '{}', and mqtt client: '{}'.",
+                    connectorName, mqttClientId);
         } catch (MqttException e) {
-            logger.error("FAILED MQTT CONNECTION for AsamMqttSourceConnectorTask: '{}, and mqtt client: '{}'.", connectorName, mqttClientId);
+            logger.error("FAILED MQTT CONNECTION for AsamMqttSourceConnectorTask: '{}', and mqtt client: '{}'.",
+                    connectorName, mqttClientId);
             logger.error(e);
         }
 
         try {
             mqttClient.subscribe(mqttTopic, connectorConfiguration.getInt("mqtt.connector.qos"));
-            logger.info("SUCCESSFULL MQTT CONNECTION for MqttSinkConnectorTask: '{}, and mqtt client: '{}'.", connectorName, mqttClientId);
+            logger.info("SUCCESSFUL MQTT SUBSCRIPTION for MqttSourceConnectorTask: '{}', and mqtt client: '{}'.",
+                    connectorName, mqttClientId);
         } catch (MqttException e) {
-            logger.error("FAILED MQTT CONNECTION for MqttSinkConnectorTask: '{}, and mqtt client: '{}'.", connectorName, mqttClientId);
+            logger.error("FAILED MQTT SUBSCRIPTION for MqttSourceConnectorTask: '{}', and mqtt client: '{}'.",
+                    connectorName, mqttClientId);
             e.printStackTrace();
         }
-
     }
 
     @Override
@@ -91,7 +102,7 @@ public class MqttSourceConnectorTask extends SourceTask implements MqttCallback 
         connectorConfiguration = new MqttSourceConnectorConfig(map);
         connectorName = connectorConfiguration.getString("mqtt.connector.kafka.name");
         kafkaTopic = connectorConfiguration.getString("mqtt.connector.kafka.topic");
-        mqttClientId = connectorConfiguration.getString("mqtt.connector.client.id");
+		mqttClientId = connectorConfiguration.getString("mqtt.connector.client.id");
         mqttTopic = connectorConfiguration.getString("mqtt.connector.broker.topic");
         logger.info("Starting AsamMqttSourceConnectorTask with connector name: '{}'", connectorName);
         initMqttClient();
@@ -106,37 +117,51 @@ public class MqttSourceConnectorTask extends SourceTask implements MqttCallback 
 
     @Override
     public void stop() {
-
+        try {
+            if (mqttClient != null && mqttClient.isConnected()) {
+                mqttClient.disconnect();
+                mqttClient.close();
+                logger.info("Disconnected and closed MQTT client for connector: '{}'.", connectorName);
+            }
+        } catch (MqttException e) {
+            logger.error("Error during MQTT disconnection for connector: '{}'.", connectorName, e);
+        }
     }
 
     @Override
     public void connectionLost(Throwable throwable) {
-        logger.error("Connection for connector: '{}', running client: '{}', lost to topic: '{}'.", connectorName, mqttClientId, mqttTopic);
+        logger.error("Connection for connector: '{}', running client: '{}', lost to topic: '{}'.", connectorName,
+                mqttClientId, mqttTopic);
+        if (throwable != null) {
+            logger.error("Reason for disconnection: {}", throwable.getMessage());
+            logger.error("Stack trace:", throwable);
+        }
     }
 
     @Override
     public void messageArrived(String tempMqttTopic, MqttMessage mqttMessage) {
-        logger.debug("Mqtt message arrived to connector: '{}', running client: '{}', on topic: '{}'.", connectorName, mqttClientId, tempMqttTopic);
+        logger.debug("Mqtt message arrived to connector: '{}', running client: '{}', on topic: '{}'.", connectorName,
+                mqttClientId, tempMqttTopic);
         try {
-            logger.debug("Mqtt message payload in byte array: '{}'", mqttMessage.getPayload());
-            //mqttRecordQueue.put(new SourceRecord(null, null, kafkaTopic, null,
-            //        Schema.STRING_SCHEMA, addTopicToJSONByteArray(mqttMessage.getPayload(), tempMqttTopic))
-            //);
-            mqttRecordQueue.put(new SourceRecord(null, null, kafkaTopic, null,
-                    Schema.STRING_SCHEMA, makeDBDoc(mqttMessage.getPayload(), tempMqttTopic))
-            );
+            byte[] payload = mqttMessage.getPayload(); // Payload as Byte-Array
+            logger.debug("Mqtt message payload: '{}'", new String(payload));
+            mqttRecordQueue.put(new SourceRecord(Collections.singletonMap("mqttTopic", tempMqttTopic),
+                    Collections.singletonMap("offset", System.currentTimeMillis()), kafkaTopic, null,
+                    Schema.BYTES_SCHEMA, payload));
         } catch (Exception e) {
-            logger.error("ERROR: Not able to create source record from mqtt message '{}' arrived on topic '{}' for client '{}'.", mqttMessage.toString(), tempMqttTopic, mqttClientId);
+            logger.error(
+                    "ERROR: Not able to create source record from mqtt message '{}' arrived on topic '{}' for client '{}'.",
+                    mqttMessage.toString(), tempMqttTopic, mqttClientId);
             logger.error(e);
         }
     }
 
     @Override
     public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
-
+        // Optional: Handle message delivery completion if needed
     }
-
-    private byte[] addTopicToJSONByteArray(byte[] bytes, String topic) {
+	
+	private byte[] addTopicToJSONByteArray(byte[] bytes, String topic) {
         String topicAsJSON = ",\"topic\":\""+topic+"\"}";
         int byteslen = bytes.length-1;
         int topiclen = topicAsJSON.length();
